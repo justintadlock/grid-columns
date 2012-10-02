24007fd89e151a675034834faf778ab1cbbaf286
Justin Tadlock <justin@justintadlock.com>
2012-10-02 18:20:53 -0500

    Update opening inline plugin docs.

--- grid-columns.php
+++ grid-columns.php
@@ -1,21 +1,33 @@
 <?php
 /*
  * Plugin Name: Grid Columns
- * Plugin URI: http://justintadlock.com
+ * Plugin URI: http://themehybrid.com/plugins/grid-columns
  * Description: A [column] shortcode plugin.
  * Version: 0.1 Beta 1
  * Author: Justin Tadlock
  * Author URI: http://justintadlock.com
  *
+ * Grid Columns was created because of the sheer number of WordPress themes adding poorly-coded 
+ * column shortcodes, which lock users into use that theme and system forever.  The plugin is 
+ * meant to be an all-around solution for any WordPress user, regardless of theme, to be able 
+ * to have columnized content using a simple [column] shortcode.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
  * @package   GridColumns
  * @version   0.1.0 Beta 1
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2012, Justin Tadlock
- * @link      http://justintadlock.com
+ * @link      http://themehybrid.com/plugins/grid-columns
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- *
- * @todo      Lots of testing with the CSS to make sure it works.
- * @todo      Possibly do some error handling for people who don't know rudimentary math.
  */
 
 class Grid_Columns {
